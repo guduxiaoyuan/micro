d16d7a3beda55841283586fe021f71bdbe95411e
赵怀远 <zhaohuaiyuan@ctsi.com.cn>
2019-11-13 15:11:06 +0800

    增加 zookeeper 分布式锁

--- cat-service/src/main/java/com/charlee/CatServiceApplication.java
+++ cat-service/src/main/java/com/charlee/CatServiceApplication.java
@@ -2,14 +2,17 @@ package com.charlee;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
-import org.springframework.cloud.openfeign.EnableFeignClients;
+//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+//import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+//import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@EnableEurekaClient
-@EnableHystrix
-@EnableFeignClients
+import tk.mybatis.spring.annotation.MapperScan;
+
+//@EnableEurekaClient
+//@EnableHystrix
+//@EnableFeignClients
 @SpringBootApplication
+@MapperScan({"com.charlee.*"})
 public class CatServiceApplication {
 
     public static void main(String[] args) {
--- cat-service/src/main/java/com/charlee/controller/CatController.java
+++ cat-service/src/main/java/com/charlee/controller/CatController.java
@@ -1,18 +1,63 @@
 package com.charlee.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.charlee.design.stragy.ImplA;
+import com.charlee.design.stragy.SimpleContext;
+import com.charlee.mapper.TestMapper;
 
 /**
  * @author charlee
  * @date 2018/5/14
  */
 @RestController
+@Scope("prototype")
 public class CatController {
-
+	
+	@Autowired
+	TestMapper testMapper;
+	
+	@Autowired
+	private UserService userService;
+	
+	@Autowired
+	SimpleContext simpleContext;
+	
+	@Autowired
+	ConfigCenterBuilder builder;
+	
+	
     @GetMapping("/hello/{name}")
     public String hello(@PathVariable String name) {
+    	User user = new User();
+    	ConfigCenterBuilder e = new ConfigCenterBuilder();
+    	
+    	ConfigCenterConfig cfg = e.namespace("a").build();
+    	
+    	System.out.println(cfg.getProtocol());
+    	
+//    	userService.register(user);
+//    	
+//    	user.setCid(01);
+//    	
+//    	
+//    	Example example = new Example(User.class);
+//    	tk.mybatis.mapper.entity.Example.Criteria c = example.createCriteria();
+//    	c.andEqualTo("cid", "01");
+//    	List l = testMapper.selectByExample(example);
+//    	System.out.println(">");
+//    	ApplicationContext beans=new ClassPathXmlApplicationContext("classpath:test.xml");    
+//    	User user=(User)beans.getBean("testbean");    
+//    	System.out.println("username:"+user.getCname());    
+//    	simpleContext.doSome("implA");
+//    	simpleContext.doSome("implB");
+    	
         return "hello, " + name;
     }
 }
